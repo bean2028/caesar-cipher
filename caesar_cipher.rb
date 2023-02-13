a1b3a91847d9b7877cafb47efd5e1bd9c35ab6d5
Eric Helstad <erichelstad@gmail.com>
2023-02-13 15:33:17 -0600

    Get input string and shift value from user

---
+++ caesar_cipher.rb
@@ -0,0 +1,9 @@
+puts "Enter a phrase to be encypted:"
+user_input = gets.chomp
+
+puts "Enter a shift value for the phrase:"
+user_shift = gets.chomp
+
+def caesar_cipher(input_string, shift)
+
+end
