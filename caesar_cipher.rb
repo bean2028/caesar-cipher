6a2ae7885157c1a34f512677a6bf9f3da3c2ebaf
Eric Helstad <erichelstad@gmail.com>
2023-02-14 10:42:43 -0600

    Add shift value to ASCII integers

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -4,10 +4,19 @@ puts "Enter a phrase to be encypted:"
 user_input = gets.chomp
 
 puts "Enter a shift value for the phrase:"
-user_shift = gets.chomp
+user_shift = gets.chomp.to_i
 
 def caesar_cipher(input_string, shift)
   array = input_string.split("").map { |char| char.ord }
+  shift_array = array.map do |num|
+    if (num <= 91 && num >= 66)
+      (num + shift - 65) % 26 + 65
+    elsif (num <= 123 && num >= 98)
+      (num + shift - 97) % 26 + 97
+    else
+      num
+    end
+  end
   binding.pry
 end
 
